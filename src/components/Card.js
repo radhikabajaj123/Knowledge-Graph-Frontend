43d4f743101c120c71ad77b10fcfb65f26279a65
radhika.bajaj <radhika.bajaj@workday.com>
2023-11-16 10:12:15 -0800

    created sample d3 graph

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,8 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Graph from '../components/Graph';
-import MultiDirectedGraphView from '../components/SigmaGraph';
+import { ForceGraph } from "../components/ForceGraph";
+import data from '../components/Data.json'
 
 const bull = (
   <Box
@@ -14,11 +15,13 @@ const bull = (
 );
 
 export default function BasicCard({nodes}) {
+    const nodeHoverTooltip = React.useCallback((node) => {
+        return `<div>${node.name}</div>`;
+      }, []);
   return (
     <Card sx={{ minWidth: 275 }} style={{backgroundColor: "#F2F1F1"}}>
       <CardContent>
-            {/* <Graph nodes={nodes}/> */}
-            <MultiDirectedGraphView/>
+            {/* <ForceGraph linksData={data.links} nodesData={data.nodes} nodeHoverTooltip={nodeHoverTooltip} /> */}
       </CardContent>
     </Card>
   );
